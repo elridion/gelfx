f1c0e8ffe95f9f062efcd07943a3aeb277344e7a
Maciej Szlosarczyk <maciej@sosek.net>
2020-10-15 15:28:47 +0300

    Add :inets to extra applications
    In Elixir 1.11, depending on applications from Erlang without
    specifying them as extra_applications in the mix file produces a
    compiler warning.

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Gelfx.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :inets]
     ]
   end
 
