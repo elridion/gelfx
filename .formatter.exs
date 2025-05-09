0fc29f22063728f58e4fc437a36856a49c6a6ff5
Michal Polit <mpolit@protonmail.com>
2025-05-09 21:51:31 +0100

    Update .formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,9 @@
-# Used by "mix format"
 [
+  plugins: [Eunomo, Styler],
+  eunomo_opts: [
+    sort_alias: true,
+    sort_import: true,
+    sort_require: true
+  ],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
